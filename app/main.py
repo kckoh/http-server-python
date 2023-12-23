09d3a5887817da8b1dc46ec1587db3526f3d4ca0
kckoh <kyungcheolkoh@gmail.com>
2023-12-22 23:20:14 -0800

    pass 2nd stage try

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept() # wait for client
 
-    server_socket.send("HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
